d460ab4e3aa40523067076d3bc2ccbc474815c8c
jiaqd1203 <jiaqd1203@gmail.com>
2021-04-02 23:57:31 +0100

    prev-next

--- Banner-Slider/Banner-Slider.js
+++ Banner-Slider/Banner-Slider.js
@@ -5,34 +5,70 @@ window.onload = function(){
     // 设置小点点居中
     var navDiv = document.getElementById("navDiv");
     var outer = document.getElementById("outer");
+    // offsetWidth布局的宽度一直到border不包括margin
     navDiv.style.left = (outer.offsetWidth - navDiv.offsetWidth)/2 + "px";
     // 默认显示图片的索引，默认选中黑色
     var index = 0;
+    var timer;
     var allA = document.getElementsByTagName("a");
     allA[index].style.backgroundColor = "black";
     // 当我执行单机响应函数的时候for循环已经执行完了
-    for(var i = 0; i<allA.length;i++){
-        
+    for(var i = 0; i<allA.length;i++){        
         // 为每一个超链接添加一个number属性
         allA[i].num = i;
         allA[i].onclick = function(){      
+            clearInterval(timer);
             // 获取点击超链接的索引，并将其设置为index
             index = this.num;
             setA();  
-            move(imgList , "left" , -520*index , 20 , function(){							
+            move(imgList , "left" , -520*index , 20 , function(){
+                autoChange();							
             });            
         }    
     }
-
-    function setA(){   
+    var prev = document.getElementById("btn-prev");
+    var next = document.getElementById("btn-next");
+        prev.onclick = function(){
+            clearInterval(timer);
+            index--;
+            move(imgList , "left" , -520*index , 20 , function(){
+                setA();
+            });                                     
+        }
+        
+        next.onclick = function(){
+            clearInterval(timer);
+            index++;
+            move(imgList , "left" , -520*index , 20 , function(){
+                setA();
+            });          
+        }
+    autoChange();
+    function setA(){        
+        if(index >= imgArr.length - 1){
+            index = 0;
+            imgList.style.left = 0;
+        }        
         for(var i=0;i<allA.length;i++){
         //这是内联样式，设置空串，内联没了，样式表生效
             allA[i].style.backgroundColor = "";
         }        
            allA[index].style.backgroundColor = "black";
+    }
+        
+        
+        function autoChange(){					
+            //开启一个定时器，用来定时去切换图片
+            timer = setInterval(function(){                
+                index++;                
+                //判断index的值,0%5=0,1%5=1,5%5=0
+                index %= imgArr.length;                
+                //执行动画，切换图片
+                move(imgList , "left" , -520*index , 20 , function(){
+                    setA();
+                });                
+            },3000);
+            
         }
-
-    setInterval(function(){
-        imgList.style.width = 520*imgArr.length + "px";
-    },1000)
+        
 }
--- Banner-Slider/tools.js
+++ Banner-Slider/tools.js
@@ -61,13 +61,5 @@ function move(obj, attr, target, speed, callback) {
  * 		name 要获取的样式名
  */
 function getStyle(obj, name) {
-
-	if(window.getComputedStyle) {
-		//正常浏览器的方式，具有getComputedStyle()方法
 		return getComputedStyle(obj, null)[name];
-	} else {
-		//IE8的方式，没有getComputedStyle()方法
-		return obj.currentStyle[name];
-	}
-
 }
